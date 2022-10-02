7942e477383349597de3734c99350cb951608a42
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-10-02 15:35:51 +0200

    hover, icons

--- restaurant-berlin/src/components/Image.js
+++ restaurant-berlin/src/components/Image.js
@@ -2,11 +2,13 @@ import React,{useState} from "react";
 
 
 export default function Image(props) {
-  const [hover, setHover]=useState(false)
+  const [hovered, setHovered]=useState(false)
 
   return (
-     <div className="image-container">
-      <img src={`./respics/${props.image}`} className="image-res" alt="restaurant" hovered={props.hovered}/>
+     <div className="image-container" onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}>
+      <img src={`./respics/${props.image}`} className="image-res" alt="restaurant"/>
+      {hovered&& <i className="ri-heart-line favorite">heart</i> }
+      {hovered &&<i className="ri-add-circle-line cart">cart</i> }
      </div>
   )
 }
